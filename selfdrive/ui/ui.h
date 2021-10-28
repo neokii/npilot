3be989ac6447ab3e9a990593bb2d9965712aa1c8
neokii <neokii@gmail.com>
2021-10-28 22:04:45 +0900

    merge changes

--- selfdrive/ui/ui.h
+++ selfdrive/ui/ui.h
@@ -80,7 +80,7 @@ typedef struct Alert {
   }
 } Alert;
 
-const Alert CONTROLS_WAITING_ALERT = {"openpilot Unavailable", "Waiting for controls to start", 
+const Alert CONTROLS_WAITING_ALERT = {"openpilot Unavailable", "Waiting for controls to start",
                                       "controlsWaiting", cereal::ControlsState::AlertSize::MID,
                                       AudibleAlert::NONE};
 
@@ -164,6 +164,7 @@ typedef struct UIState {
   UIScene scene = {};
 
   bool awake;
+  bool has_prime = false;
 
   float car_space_transform[6];
   bool wide_camera;
