2ca59c72ffb49414df53fc3840d9a1d7d73e2d4d
neokii <neokii@gmail.com>
2021-12-06 16:47:58 +0900

    tune scc

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -72,7 +72,7 @@ class CarInterface(CarInterfaceBase):
     ret.longitudinalActuatorDelayUpperBound = 0.2
 
     ret.startAccel = -0.8
-    ret.stopAccel = -2.0
+    ret.stopAccel = -2.5
     ret.startingAccelRate = 5.0  # brake_travel/s while releasing on restart
     ret.stoppingDecelRate = 0.3  # brake_travel/s while trying to stop
     ret.vEgoStopping = 0.6
