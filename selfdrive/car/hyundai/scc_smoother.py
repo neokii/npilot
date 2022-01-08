08a8fc32618c82d98beee06bdf255038769d4f16
neokii <neokii@gmail.com>
2022-01-08 23:23:35 +0900

    update curve speed

--- selfdrive/car/hyundai/scc_smoother.py
+++ selfdrive/car/hyundai/scc_smoother.py
@@ -294,7 +294,7 @@ class SccSmoother:
 
   def cal_curve_speed(self, sm, v_ego, frame):
 
-    if frame % 20 == 0:
+    if frame % 10 == 0:
       md = sm['modelV2']
       if len(md.position.x) == TRAJECTORY_SIZE and len(md.position.y) == TRAJECTORY_SIZE:
         x = md.position.x
@@ -303,7 +303,7 @@ class SccSmoother:
         d2y = np.gradient(dy, x)
         curv = d2y / (1 + dy ** 2) ** 1.5
 
-        start = int(interp(v_ego, [10., 27.], [10, TRAJECTORY_SIZE-10]))
+        start = int(interp(v_ego, [10., 27.], [5, TRAJECTORY_SIZE-10]))
         curv = curv[start:min(start+10, TRAJECTORY_SIZE)]
         a_y_max = 2.975 - v_ego * 0.0375  # ~1.85 @ 75mph, ~2.6 @ 25mph
         v_curvature = np.sqrt(a_y_max / np.clip(np.abs(curv), 1e-4, None))
