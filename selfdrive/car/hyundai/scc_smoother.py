13212017366c0ab119632132efd629f985f981d5
neokii <neokii@gmail.com>
2022-02-11 15:05:16 +0900

    fix clu11 counter

--- selfdrive/car/hyundai/scc_smoother.py
+++ selfdrive/car/hyundai/scc_smoother.py
@@ -108,10 +108,10 @@ class SccSmoother:
     self.slowing_down_sound_alert = False
 
   @staticmethod
-  def create_clu11(packer, frame, bus, clu11, button):
+  def create_clu11(packer, bus, clu11, button):
     values = copy.copy(clu11)
     values["CF_Clu_CruiseSwState"] = button
-    values["CF_Clu_AliveCnt1"] = frame
+    values["CF_Clu_AliveCnt1"] = (values["CF_Clu_AliveCnt1"] + 1) % 0x10
     return packer.make_can_msg("CLU11", bus, values)
 
   def is_active(self, frame):
@@ -233,7 +233,7 @@ class SccSmoother:
 
       if self.btn != Buttons.NONE:
 
-        can_sends.append(SccSmoother.create_clu11(packer, self.alive_timer, CS.scc_bus, CS.clu11, self.btn))
+        can_sends.append(SccSmoother.create_clu11(packer, CS.scc_bus, CS.clu11, self.btn))
 
         if self.alive_timer == 0:
           self.started_frame = frame
