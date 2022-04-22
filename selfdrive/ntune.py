dc6195b3f6c0ae48e872daba63f7e46266e5c0cc
neokii <neokii@gmail.com>
2022-04-23 00:51:24 +0900

    change default

--- selfdrive/ntune.py
+++ selfdrive/ntune.py
@@ -221,7 +221,7 @@ class nTune():
       updated = True
     if self.checkValue("maxLatAccel", 1.0, 4.0, 2.5):
       updated = True
-    if self.checkValue("friction", 0.0, 0.2, 0.06):
+    if self.checkValue("friction", 0.0, 0.2, 0.01):
       updated = True
 
     return updated
