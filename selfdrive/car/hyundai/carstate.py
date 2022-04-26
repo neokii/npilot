526913ea310b500dd49cdda1a7a1e8ca4f66e418
neokii <neokii@gmail.com>
2022-04-26 22:04:56 +0900

    long tuning

--- selfdrive/car/hyundai/carstate.py
+++ selfdrive/car/hyundai/carstate.py
@@ -1,4 +1,5 @@
 from cereal import car
+from common.numpy_fast import interp
 from selfdrive.car.hyundai.values import DBC, STEER_THRESHOLD, FEATURES, CAR, HYBRID_CAR, EV_HYBRID_CAR
 from selfdrive.car.interfaces import CarStateBase
 from opendbc.can.parser import CANParser
@@ -84,11 +85,12 @@ class CarState(CarStateBase):
       cp.vl["WHL_SPD11"]["WHL_SPD_RR"],
     )
 
-    vEgoRawWheel = (ret.wheelSpeeds.fl + ret.wheelSpeeds.fr + ret.wheelSpeeds.rl + ret.wheelSpeeds.rr) / 4.
-    vEgoWheel, aEgoWheel = self.update_speed_kf(vEgoRawWheel)
-
     vEgoRawClu = cluSpeed * self.speed_conv_to_ms
     vEgoClu, aEgoClu = self.update_clu_speed_kf(vEgoRawClu)
+
+    vEgoRawWheel = (ret.wheelSpeeds.fl + ret.wheelSpeeds.fr + ret.wheelSpeeds.rl + ret.wheelSpeeds.rr) / 4.
+    vEgoRawWheel = interp(vEgoRawWheel, [0., 10.], [(vEgoRawWheel + vEgoRawClu) / 2., vEgoRawWheel])
+    vEgoWheel, aEgoWheel = self.update_speed_kf(vEgoRawWheel)
 
     if self.use_cluster_speed:
       ret.vEgoRaw = vEgoRawClu
