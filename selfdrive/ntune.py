99605db25f38000953fc15d5baedffd5f5c63cc5
neokii <neokii@gmail.com>
2022-05-08 21:04:38 +0900

    update ntune

--- selfdrive/ntune.py
+++ selfdrive/ntune.py
@@ -223,11 +223,13 @@ class nTune():
 
     if self.checkValue("useSteeringAngle", 0., 1., 1.):
       updated = True
-    if self.checkValue("maxLatAccel", 0.5, 4.0, 1.8):
+    if self.checkValue("maxLatAccel", 0.5, 4.0, 2.0):
       updated = True
     if self.checkValue("friction", 0.0, 0.2, 0.01):
       updated = True
-    if self.checkValue("kd", 0.0, 1.0, 0.0):
+    if self.checkValue("ki_factor", 0.0, 1.0, 0.1):
+      updated = True
+    if self.checkValue("kd", 0.0, 2.0, 0.0):
       updated = True
     if self.checkValue("deadzone", 0.0, 0.05, 0.0):
       updated = True
@@ -275,7 +277,7 @@ class nTune():
       max_lat_accel = float(self.config["maxLatAccel"])
       torque.pid._k_p = [[0], [1.0 / max_lat_accel]]
       torque.pid.k_f = 1.0 / max_lat_accel
-      torque.pid._k_i = [[0], [0.1 / max_lat_accel]]
+      torque.pid._k_i = [[0], [self.config["ki_factor"] / max_lat_accel]]
       torque.pid._k_d = [[0], [float(self.config["kd"])]]
       torque.friction = float(self.config["friction"])
       torque.deadzone = float(self.config["deadzone"])
