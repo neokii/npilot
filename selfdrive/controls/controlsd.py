7e0b4499f949969b6d94236cb537c4803d488393
neokii <neokii@gmail.com>
2022-07-06 17:23:37 +0900

    reduce warning

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -640,7 +640,7 @@ class Controls:
 
     # Send a "steering required alert" if saturation count has reached the limit
     if lac_log.active and not CS.steeringPressed and self.CP.lateralTuning.which() == 'torque':
-      undershooting = abs(lac_log.desiredLateralAccel) / abs(1e-3 + lac_log.actualLateralAccel) > 1.2
+      undershooting = abs(lac_log.desiredLateralAccel) / abs(1e-3 + lac_log.actualLateralAccel) > 1.3
       turning = abs(lac_log.desiredLateralAccel) > 1.0
       good_speed = CS.vEgo > 5
       max_torque = abs(self.last_actuators.steer) > 0.99
