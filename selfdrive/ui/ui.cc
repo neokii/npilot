e9bfb0936c80707a88b3e19d1b1515f8349349a4
neokii <neokii@gmail.com>
2022-07-19 11:56:04 +0900

    merge changes

--- selfdrive/ui/ui.cc
+++ selfdrive/ui/ui.cc
@@ -195,8 +195,9 @@ static void update_state(UIState *s) {
 }
 
 void ui_update_params(UIState *s) {
-  Params params;
+  auto params = Params();
   s->scene.is_metric = params.getBool("IsMetric");
+  s->scene.map_on_left = params.getBool("NavSettingLeftSide");
   s->show_debug = params.getBool("ShowDebugUI");
   s->lat_control = std::string(Params().get("LateralControl"));
 }
